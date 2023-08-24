13c445850a98fd7f2eaa484d1ddeb07dba010580
Bazir10 <enydra10@gmail.com>
2023-08-24 04:36:22 +0200

    utilisation de document.getElementById sur Js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,2 +1,16 @@
-fetch("https://api.github.com/users/Bazir10")
-.then(reponse => console.log(reponse.json())
+fetch("http://localhost:5678/api/works")
+.then(reponse => console.log(reponse.json()))
+var element = document.getElementById(portfolio).innerText = "Mes Projets";
+
+var element = document.getElementById(contact).innerText = "contact";
+
+var element = document.getElementById(name).addEventListener("click", function () {
+    alert("vous avez cliqué !");
+});
+var element = document.getElementById(email).addEventListener("click", function () {
+    alert("vous avez cliqué !");
+});
+var element = document.getElementById(message).addEventListener("click", function () {
+    alert("vous avez cliqué !");
+});
+
